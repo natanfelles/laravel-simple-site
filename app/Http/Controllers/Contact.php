ac513a7b0c0e32608f3ee4016641fdda759a7655
Natan Felles <natanfelles@gmail.com>
2020-07-08 15:48:43 -0300

    Add contact.sent translation

--- app/Http/Controllers/Contact.php
+++ app/Http/Controllers/Contact.php
@@ -9,20 +9,20 @@ class Contact extends Controller
 {
 	public function create()
 	{
-		return view('contact.create',[
-			'msg' => session('msg')
+		return view('contact.create', [
+			'msg' => session('msg'),
 		]);
 	}
 
 	public function store(ContactStoreRequest $request)
 	{
-		$data =  $request->only('name', 'email', 'subject', 'message');
+		$data = $request->only('name', 'email', 'subject', 'message');
 
 		ContactModel::create($data);
 
 		return redirect()->route('contact.create')->with('msg', [
 			'type' => 'success',
-			'contents' => 'Contact successful sent.',
+			'contents' => __('contact.sent'),
 		]);
 	}
 }
